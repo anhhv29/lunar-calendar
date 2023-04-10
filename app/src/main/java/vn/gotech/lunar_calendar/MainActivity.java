dbaafe82299f08a5893c638cb5d79f31f44c2bd1
anhhv <anhhv@gotech.vn>
2023-04-10 17:34:25 +0700

    Update UI

--- app/src/main/java/vn/gotech/lunar_calendar/MainActivity.java
+++ app/src/main/java/vn/gotech/lunar_calendar/MainActivity.java
@@ -1,8 +1,8 @@
 package vn.gotech.lunar_calendar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
